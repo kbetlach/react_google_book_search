c5d2cfbab14e753dbc2c48779fa5b03c018ab3e1
kbetlach <kbetlach@gmail.com>
2020-05-07 13:50:20 -0500

    fixed navtab placement on smaller screens with media queries

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,10 +6,10 @@ function Nav() {
     <nav className="navbar navbar-expand-lg bg-dark text-light">
         <h3>Google Books</h3>
 
-        <a className="nav-link" href="/">
+        <a className="nav-link" href="/" id="searchTab">
         Book Search
       </a>
-      <a className="nav-link" href="/SavedBooks">
+      <a className="nav-link" href="/SavedBooks" id="savedTab">
         Saved Books
       </a>
 
